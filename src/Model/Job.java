34ac3617dcab731978a1e56f686699762f6c66f1
muhammadreza <mra.akhgari@gmail.com>
2019-12-20 02:18:43 +0330

    Main added

--- src/Model/Job.java
+++ src/Model/Job.java
@@ -1,7 +1,7 @@
 package Model;
 
 /**
- * Model.Job class that contains information of Model.Job.
+ * Job class that contains information of Job.
  * fields of class are:
  * id: A unique integer to find a job and check whether it is equivalent to another job.
  * arrivalTime: a byte (range in 1 to 50) that show the arrival time of job
@@ -17,10 +17,12 @@ public class Job implements Comparable<Job> {
     private byte arrivalTime;
     private byte burstTime;
     private int startTime;
+    private boolean isActive;
 
     /**
-     * Constructs a newly allocated {@code Model.Job} object that
+     * Constructs a newly allocated {@code Job} object that
      * represents the specified {@code int} id ,{@code byte} arrivalTime ,{@code byte} burstTime and {@code int} startTime.
+     *
      * @param id          a unique int.
      * @param arrivalTime a byte that shows arrival time of job.
      * @param burstTime   a byte that shows burst time of job.
@@ -31,30 +33,42 @@ public class Job implements Comparable<Job> {
         this.burstTime = burstTime;
     }
 
-    public Integer getId() {
+    Integer getId() {
         return id;
     }
 
-    public byte getArrivalTime() {
+    byte getArrivalTime() {
         return arrivalTime;
     }
 
-    public byte getBurstTime() {
+    byte getBurstTime() {
         return burstTime;
     }
 
-    public int getStartTime() {
+    int getStartTime() {
         return startTime;
     }
 
-    public void setStartTime(int startTime) {
+    boolean isActive() {
+        return isActive;
+    }
+
+    void setActive() {
+        isActive = true;
+    }
+
+    void setDeActive() {
+        isActive = false;
+    }
+
+    void setStartTime(int startTime) {
         this.startTime = startTime;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Job)
-            return ((Job) obj).getId() == this.id;
+            return ((Job) obj).getId().equals(this.id);
         return false;
     }
 
@@ -68,30 +82,30 @@ public class Job implements Comparable<Job> {
     }
 
     /**
-     * Checks if the two job {@code Model.Job} ,{ @code Model.Job} have time interference?
+     * Checks if the two job {@code Job} ,{ @code Job} have time interference?
      *
      * @param j1 first job.
      * @param j2 second job.
      * @return {@code true} if the objects have time interference;
-     *         {@code false} otherwise;
+     * {@code false} otherwise;
      */
-    public static boolean hasOverlap(Job j1, Job j2) {
+    private static boolean hasOverlap(Job j1, Job j2) {
         return j1.getStartTime() < j2.getStartTime() + j2.getBurstTime() &&
                 j2.getStartTime() < j1.getStartTime() + j1.getBurstTime();
     }
 
     /**
-     * Compares two {@code Model.Job} objects numerically.
+     * Compares two {@code Job} objects numerically.
      *
-     * @param o the {@code Model.Job} to be compared.
-     * @return the value {@code 0} if this {@code Model.Job} is
-     *          equal to the argument {@code Model.Job}; a value less than
-     *          {@code 0} if this.arrivalTime {@code Integer} is numerically less
-     *          than the argument.arrivalTime {@code Integer} or if this.arrivalTime {@code Integer} is equal to
-     *          argument.arrivalTime and this.burstTime {@code Integer} is numerically less
-     *          than the argument.burstTime {@code Integer};
-     *          and a value greater than {@code 0} if this.arrivalTime {@code Integer} is numerically
-     *          greater than the argument.arrivalTime {@code Integer}.
+     * @param o the {@code Job} to be compared.
+     * @return the value {@code 0} if this {@code Job} is
+     * equal to the argument {@code Job}; a value less than
+     * {@code 0} if this.arrivalTime {@code Integer} is numerically less
+     * than the argument.arrivalTime {@code Integer} or if this.arrivalTime {@code Integer} is equal to
+     * argument.arrivalTime and this.burstTime {@code Integer} is numerically less
+     * than the argument.burstTime {@code Integer};
+     * and a value greater than {@code 0} if this.arrivalTime {@code Integer} is numerically
+     * greater than the argument.arrivalTime {@code Integer}.
      */
     @Override
     public int compareTo(Job o) {
@@ -102,13 +116,10 @@ public class Job implements Comparable<Job> {
         return x1 == y1 ? Integer.compare(x2, y2) : Integer.compare(y1, x1);
     }
 
-//    public static int compareIds(Model.Job o){
-//        return compare(this.getId(), o.getId(), 0,0);
-//    }
 
     @Override
     public String toString() {
-        return "(id: " + this.id + " arrivalTime: " + this.arrivalTime + " burstTime: " + this.burstTime + ")" ;
-//        return  "" + this.startTime;
+//        return "(id: " + this.id + " exe time: " + this.startTime + " burstTime: " + this.burstTime + ")";
+        return  "( " + this.startTime + ", " + this.burstTime + ")";
     }
 }
