0e3c79e0dd011395c92c449e1fe38f22f0871d83
mohamadHasan <mhtaghad99@gmail.com>
2019-12-21 07:37:11 +0330

    start writting handler

--- src/Checker/FileHandler.java
+++ src/Checker/FileHandler.java
@@ -1,6 +1,9 @@
 package Checker;
 import Model.Job;
 
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * @author MohamadHasan Taghadosi
  * @date
@@ -8,7 +11,33 @@ import Model.Job;
 
 public class FileHandler {
 
-    public FileHandler(){
+
+    public FileHandler() throws FileNotFoundException {
+
+    }
+
+
+    public ArrayList<Job> handleInputFile() throws IOException {
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        String oneLineOfFile;
+        String[] resOneLineOfFile;
+        int lineNum = 0;
+        File file = new File("./Checker/inputFile.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        oneLineOfFile = br.readLine();
+        lineNum = Integer.parseInt(oneLineOfFile);
+        while ((oneLineOfFile = br.readLine()) != null) {
+            if (oneLineOfFile.equals("")) {
+                break;
+            }
+            resOneLineOfFile = oneLineOfFile.split(" ");
+
+
+
+
+        }
+
+
     }
 
 }
